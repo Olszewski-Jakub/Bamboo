a10271374eee145b3400fca9c2a2e0fbafa2341e
Jakub Olszewski <j.olszewski05@gmail.com>
2023-10-24 11:33:04 +0100

    Initilised spring boot

---
+++ src/main/java/live/olszewski/bamboo/BambooApplication.java
@@ -0,0 +1,13 @@
+package live.olszewski.bamboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/olszewski/bamboo/BambooApplicationTests.java
@@ -0,0 +1,13 @@
+package live.olszewski.bamboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
