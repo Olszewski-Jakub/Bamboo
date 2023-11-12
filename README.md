3047b9abb2f38c48db5149b9c22c29f08cf6c5d8
Jakub Olszewski <j.olszewski05@gmail.com>
2023-11-12 20:06:02 +0000

    Implemented Swagger. Information added only for User related endpoints

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TODO
-
+[Swagger](http://localhost:8080/swagger-ui/index.html#/)
 - User
   - User Authorisation
   - Add, delete userDao 
--- src/main/java/live/olszewski/bamboo/user/UserController.java
+++ src/main/java/live/olszewski/bamboo/user/UserController.java
@@ -1,5 +1,10 @@
 package live.olszewski.bamboo.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -7,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "#{apiConfig.userPath}")
-
+@Tag(name = "User", description = "User related endpoints")
 public class UserController {
 
     private final UserService userService;
@@ -18,17 +23,83 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(
+            description = "Get all users",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Unauthorized"
+                    )
+            })
     public List<UserDao> getStudents() {
         return userService.getUsers();
     }
 
     @PostMapping
-    public void registerNewUser(@RequestBody RegisterUser user){
+    @Operation(
+            description = "Register new user",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "User already exists"
+                    )
+            }
+    )
+    @Parameter(
+            name = "user",
+            description = "User to register",
+            required = true,
+            example = "{\n" +
+                    "  \"name\": \"John\",\n" +
+                    "  \"surname\": \"Doe\",\n" +
+                    "  \"email\": \"john.doe@gmail.com\",\n" +
+                    "  \"password\": \"password\"\n" +
+                    "}"
+    )
+    public void registerNewUser(@RequestBody RegisterUser user) {
         userService.addNewUser(user);
     }
 
     @DeleteMapping(path = "{userId}")
-    public void deleteUser(@PathVariable("userId") Long id){
+    @Operation(
+            description = "Delete user",
+            security = @SecurityRequirement(name = "bearerAuth"),
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Success"
+                    ),
+                    @ApiResponse(
+                            responseCode = "403",
+                            description = "Unauthorized"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "User does not exists"
+                    )
+            }
+    )
+    @Parameter(
+            name = "userId",
+            description = "User id",
+            required = true,
+            example = "1"
+    )
+    public void deleteUser(@PathVariable("userId") Long id) {
         userService.deleteUser(id);
     }
 }
