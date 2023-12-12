e7c8f3475165850c196049b9138d0f293119dce8
Jakub Olszewski <j.olszewski05@gmail.com>
2023-12-12 20:57:02 +0000

    Successfully launched app in docker containers

---
+++ src/test/java/live/olszewski/bamboo/JsonExporterTest.java
@@ -0,0 +1,49 @@
+package live.olszewski.bamboo;
+
+import live.olszewski.bamboo.panda.config.PandaConfigDto;
+import live.olszewski.bamboo.services.jsonExporter.JsonExporterService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class JsonExporterTest {
+    @Autowired
+    JsonExporterService jsonExporterService;
+
+    @Test
+    public void exportReturnsCorrectJsonForString() {
+        String input = "test";
+        String expectedJson = "\"test\"";
+        String actualJson = jsonExporterService.export(input);
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    public void exportReturnsCorrectJsonForInteger() {
+        Integer input = 123;
+        String expectedJson = "123";
+        String actualJson = jsonExporterService.export(input);
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    public void exportReturnsCorrectJsonForBoolean() {
+        Boolean input = true;
+        String expectedJson = "true";
+        String actualJson = jsonExporterService.export(input);
+        assertEquals(expectedJson, actualJson);
+    }
+
+    @Test
+    public void exportReturnsCorrectJsonForPandaConfigDto() {
+        PandaConfigDto input = new PandaConfigDto("uuid", "location", "name", "owner", "api_key");
+        String expectedJson = "{\"uuid\":\"uuid\",\"location\":\"location\",\"name\":\"name\",\"owner\":\"owner\",\"api_key\":\"api_key\"}";
+        String actualJson = jsonExporterService.export(input);
+        assertEquals(expectedJson, actualJson);
+    }
+}
---
+++ src/test/java/live/olszewski/bamboo/UUIDServiceTest.java
@@ -0,0 +1,37 @@
+package live.olszewski.bamboo;
+
+import live.olszewski.bamboo.services.uuid.UUIDService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+public class UUIDServiceTest {
+
+    @Autowired
+    UUIDService uuidService;
+
+    @Test
+    public void generateUUIDFromStringReturnsSameUUIDForSameInput() {
+        String input = "testInput";
+        UUID uuid1 = uuidService.generateUUIDFromString(input);
+        UUID uuid2 = uuidService.generateUUIDFromString(input);
+        assertEquals(uuid1, uuid2);
+    }
+
+    @Test
+    public void generateUUIDFromStringReturnsDifferentUUIDForDifferentInput() {
+        String input1 = "testInput1";
+        String input2 = "testInput2";
+        UUID uuid1 = uuidService.generateUUIDFromString(input1);
+        UUID uuid2 = uuidService.generateUUIDFromString(input2);
+        assertNotEquals(uuid1, uuid2);
+    }
+
+}
