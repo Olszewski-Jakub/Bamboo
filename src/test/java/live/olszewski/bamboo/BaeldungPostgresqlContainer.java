01f34d4c1eed720aa966adf3c5df7feb005efbe7
Jakub Olszewski <j.olszewski05@gmail.com>
2023-12-16 19:20:05 +0000

    trying to fix test execution on gh action
    Signed-off-by: Jakub Olszewski <j.olszewski05@gmail.com>

--- src/test/java/live/olszewski/bamboo/BaeldungPostgresqlContainer.java
+++ src/test/java/live/olszewski/bamboo/BaeldungPostgresqlContainer.java
@@ -1,7 +1,5 @@
 package live.olszewski.bamboo;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 /**
@@ -10,57 +8,33 @@ import org.testcontainers.containers.PostgreSQLContainer;
  * This class is used to manage a Docker container for PostgreSQL database for integration tests.
  */
 public class BaeldungPostgresqlContainer extends PostgreSQLContainer<BaeldungPostgresqlContainer> {
-    // The version of the PostgreSQL Docker image to be used
     private static final String IMAGE_VERSION = "postgres:16";
-    // The single instance of this class
     private static BaeldungPostgresqlContainer container;
 
-    /**
-     * Private constructor to prevent creating multiple instances.
-     * Calls the super constructor with the PostgreSQL Docker image version.
-     */
     private BaeldungPostgresqlContainer() {
         super(IMAGE_VERSION);
+        withDatabaseName("postgre-test");
+        withUsername("postgre");
+        withPassword("admin");
+        withInitScript("init.sql");
     }
 
-    /**
-     * This method returns the single instance of this class.
-     * If the instance does not exist, it is created.
-     *
-     * @return the single instance of BaeldungPostgresqlContainer
-     */
     public static BaeldungPostgresqlContainer getInstance() {
         if (container == null) {
             container = new BaeldungPostgresqlContainer();
-            container.withDatabaseName("integration-tests-db")
-                    .withUsername("postgres")
-                    .withPassword("admin")
-                    .withInitScript("init.sql");
-
+            container.start();
         }
         return container;
     }
 
-    @DynamicPropertySource
-    static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
-        registry.add("spring.datasource.username", container::getUsername);
-        registry.add("spring.datasource.password", container::getPassword);
-    }
-    /**
-     * This method starts the PostgreSQL Docker container and sets the system properties
-     * for the database URL, username, and password.
-     */
     @Override
     public void start() {
         super.start();
-        container.start();
+        System.setProperty("DB_URL", getJdbcUrl());
+        System.setProperty("DB_USERNAME", getUsername());
+        System.setProperty("DB_PASSWORD", getPassword());
     }
 
-    /**
-     * This method overrides the stop method from the super class.
-     * The container is not stopped manually, the JVM handles the shutdown.
-     */
     @Override
     public void stop() {
         //do nothing, JVM handles shut down
