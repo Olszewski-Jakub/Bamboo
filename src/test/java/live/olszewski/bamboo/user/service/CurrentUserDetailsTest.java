035388ea79eabc48ef52d243aaf3fce7255d5f50
Jakub Olszewski <j.olszewski05@gmail.com>
2023-12-16 18:57:53 +0000

    trying to fix test execution on gh action
    Signed-off-by: Jakub Olszewski <j.olszewski05@gmail.com>

--- src/test/java/live/olszewski/bamboo/user/service/CurrentUserDetailsTest.java
+++ src/test/java/live/olszewski/bamboo/user/service/CurrentUserDetailsTest.java
@@ -13,14 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
 public class CurrentUserDetailsTest {
 
     @Autowired
