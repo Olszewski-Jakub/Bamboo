8ce25544b67e71d4ea5f10b66a3f45b17d2cefb5
Jakub Olszewski <j.olszewski05@gmail.com>
2023-12-16 19:03:27 +0000

    trying to fix test execution on gh action
    Signed-off-by: Jakub Olszewski <j.olszewski05@gmail.com>

--- src/test/java/live/olszewski/bamboo/panda/register/service/AddPandaDeviceServiceTest.java
+++ src/test/java/live/olszewski/bamboo/panda/register/service/AddPandaDeviceServiceTest.java
@@ -8,17 +8,18 @@ import live.olszewski.bamboo.testUtils.TestUtils;
 import org.junit.ClassRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
-@Testcontainers
+@ActiveProfiles({"tc", "tc-auto"})
 public class AddPandaDeviceServiceTest {
 
     @Autowired
@@ -29,6 +30,7 @@ public class AddPandaDeviceServiceTest {
 
     @Autowired
     private PandaRepository pandaRepository;
+
 
     @ClassRule
     public static PostgreSQLContainer<BaeldungPostgresqlContainer> postgreSQLContainer = BaeldungPostgresqlContainer.getInstance();
