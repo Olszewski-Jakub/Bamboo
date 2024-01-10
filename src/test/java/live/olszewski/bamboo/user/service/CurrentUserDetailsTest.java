ab3bc3f4bb55a82004bec382681c30e2e241a3e8
Jakub Olszewski <j.olszewski05@gmail.com>
2024-01-10 16:19:05 +0000

    Fixed tests for CurrentUserDetailsTest
    Signed-off-by: Jakub Olszewski <j.olszewski05@gmail.com>

--- src/test/java/live/olszewski/bamboo/user/service/CurrentUserDetailsTest.java
+++ src/test/java/live/olszewski/bamboo/user/service/CurrentUserDetailsTest.java
@@ -19,7 +19,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -35,7 +35,6 @@ public class CurrentUserDetailsTest {
 
     @Autowired
     private TestUtils testUtils;
-
 
     @Container
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
@@ -77,6 +76,8 @@ public class CurrentUserDetailsTest {
 
     @Test
     public void currentUserDetails_ThrowsExceptionWhenUserDoesNotExist() {
-        assertThrows(IllegalStateException.class, () -> userService.currentUserDetails());
+        ResponseEntity<ApiResponseDto<?>> reponse = userService.currentUserDetails();
+        assertEquals(404, reponse.getBody().getStatusCode());
+        assertEquals("User with email user1@test.com does not exist", reponse.getBody().getMessage());
     }
 }
